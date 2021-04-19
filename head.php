ec3b1a44de18abede380ae157c5169dea4d55ecf
J4N8 <43821141+J4N8@users.noreply.github.com>
2021-04-19 12:51:25 +0200

    Fixed index.php's layout Added more options to head.php

--- head.php
+++ head.php
@@ -3,12 +3,10 @@ require_once 'classes/Database.php';
 require_once 'classes/Product.php';
 require_once 'classes/ShoppingCart.php';
 ?>
-<!--TODO: Add more options-->
 <table>
     <tr>
-        <!-- Just a placeholder for now -->
         <th><a href="productPage.php">Products</a></th>
-        <th><a href=""></a></th>
-        <th><a href=""></a></th>
+        <th><a href="boughtProductsStats.php">Bought Products</a></th>
+        <th><a href="categoryStats.php">Category Product Count</a></th>
     </tr>
 </table>
